c3eb79d60296acb1d95c2c51c69ba768f6df26fe
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-03-29 18:21:45 -0400

    Refactor intial server start up

--- src/server.js
+++ src/server.js
@@ -1,13 +1,15 @@
 // Server start-up dependencies
 const PORT = 8080;
 const http = require('http');
-const server = http.createServer(function(request, response) {
-  response.writeHead(200, 
-    {"Content-Type": "text/plain"});
-    
+
+const serverResponse = (request, response) => {
+  response.writeHead(200, {"Content-Type": "text/plain"});
   response.end("Hello World\n");
-  });
+}
+
+const server = http.createServer(serverResponse);
 
 
-  server.listen(PORT);
+server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
+});
