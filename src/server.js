5ff1ab1e0d6e4abb429e23387561fd3338596e87
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-03-31 17:57:05 -0400

    Define route files

--- src/server.js
+++ src/server.js
@@ -3,10 +3,18 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+// Const Define routes
+
+const root = require("./routes/root");
+const login = require("./routes/login");
+const signup = require("./routes/signup")
+
+
 // Define first route and server response
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
+
 
 
 // Start the server and log that it's running
